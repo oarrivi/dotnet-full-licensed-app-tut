ec340a70b230e0451f09c22e636ba0bc6209878d
oarrivi <oarrivi@gmail.com>
2015-02-21 11:46:00 +0100

    Add Console App Project without licensing

---
+++ Simple Licensed Application Tutorial/MyLicensedConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLicensedConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------------------------------------------------------");
+            Console.WriteLine("Este es un ejemplo de licenciamiento de una aplicación.");
+            Console.WriteLine("-------------------------------------------------------------");
+
+            SystemDateTimeProvider provider = new SystemDateTimeProvider();
+            DateTime dateTime = provider.GetDateTime();
+            Console.WriteLine(" Fecha y hora del sistema: {0}", dateTime);
+
+            Console.WriteLine("-------------------------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("Pulse una tecla para terminar...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Simple Licensed Application Tutorial/MyLicensedConsoleApp/SystemDateTimeProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLicensedConsoleApp
+{
+    public class SystemDateTimeProvider
+    {
+        public SystemDateTimeProvider()
+        {
+        }
+
+        public DateTime GetDateTime()
+        {
+            return System.DateTime.Now;
+        }
+
+        public void WriteDateTime()
+        {
+            Console.WriteLine("{0}", this.GetDateTime());
+        }
+    }
+}
